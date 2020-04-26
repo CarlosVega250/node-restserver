473aa58737b70f7b701bc04f994de2797b7c7c1d
CarlosVega250 <csvega250@gmail.com>
2020-04-26 01:47:31 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://carlosvega250:carsol9812162909129812@sandbox-ipkqn.mongodb.net/test';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
